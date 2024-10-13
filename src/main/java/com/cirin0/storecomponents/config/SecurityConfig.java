bc6e9b2c0d281dbd181de89f0abfee9183752607
cirin0 <ivan.gruziv@gmail.com>
2024-10-13 21:42:16 +0300

    ref and add: configs

--- src/main/java/com/cirin0/storecomponents/config/SecurityConfig.java
+++ src/main/java/com/cirin0/storecomponents/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.cirin0.storecomponents.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,15 +12,28 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
+@AllArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
+  private final UserDetailsServiceImpl userDetailsService;
+
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
+  public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(userDetailsService);
+    provider.setPasswordEncoder(new BCryptPasswordEncoder());
+    return provider;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
         .headers(headers -> headers
@@ -29,11 +44,16 @@ public class SecurityConfig {
                 "products/category/{categoryId}", "/category", "/card/del", "/card/add", "/h2-console/**",
                 "/auth/**", "/register", "/register-form", "/login-form"
             ).permitAll()
-            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .requestMatchers("/", "/api/auth/**", "/api/categories", "/api/products").permitAll()
+            .requestMatchers("/admin/**", "/api/users/admin/**", "/api/categories/").hasRole("ADMIN")
             .anyRequest().permitAll())
-        .formLogin(Customizer.withDefaults())
+//        .formLogin(Customizer.withDefaults())
+
+        .formLogin(form -> form
+            .loginPage("/auth/login")
+            .permitAll())
         .httpBasic(AbstractHttpConfigurer::disable)
-        .userDetailsService(userDetailsService)
+        //.userDetailsService(userDetailsService)
         .logout(Customizer.withDefaults());
 
     return http.build();
---
+++ src/main/java/com/cirin0/storecomponents/config/UserDetailsImpl.java
@@ -0,0 +1,74 @@
+package com.cirin0.storecomponents.config;
+
+import com.cirin0.storecomponents.model.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class UserDetailsImpl implements UserDetails {
+
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+  private Collection<? extends GrantedAuthority> authorities;
+
+  public UserDetailsImpl(String firstName, String lastName, String email, String password,
+                         Collection<? extends GrantedAuthority> authorities) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.authorities = authorities;
+  }
+
+  public static UserDetailsImpl build(User user) {
+    List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    return new UserDetailsImpl(
+        user.getFirstName(),
+        user.getLastName(),
+        user.getEmail(),
+        user.getPassword(),
+        authorities);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return UserDetails.super.isAccountNonExpired();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return UserDetails.super.isAccountNonLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return UserDetails.super.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return UserDetails.super.isEnabled();
+  }
+}

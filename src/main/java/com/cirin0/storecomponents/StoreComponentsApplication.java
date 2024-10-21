9c9fa700f41248906a1bbd625d5b5fab92a5c37f
cirin0 <ivan.gruziv@gmail.com>
2024-10-21 21:47:50 +0300

    for heroku deploy

--- src/main/java/com/cirin0/storecomponents/StoreComponentsApplication.java
+++ src/main/java/com/cirin0/storecomponents/StoreComponentsApplication.java
@@ -2,10 +2,8 @@ package com.cirin0.storecomponents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Profile({"prod", "default", "dev"})
 public class StoreComponentsApplication {
   public static void main(String[] args) {
     SpringApplication.run(StoreComponentsApplication.class, args);
